52a4ea79c594d558479ff8cbb21aafa1e7ef0e7a
suvomoy <sma71@3ds.com>
2024-06-14 13:01:14 +0530

    [1:1] Add TSP calculation logic

--- main.go
+++ main.go
@@ -9,6 +9,72 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+const MAX = 1e9
+
+func tsp(dist [][]float64) (float64, []int) {
+	n := len(dist)
+	memo := make([][]float64, n)
+	for i := range memo {
+		memo[i] = make([]float64, 1<<n)
+		for j := range memo[i] {
+			memo[i][j] = -1
+		}
+	}
+
+	// dp function to find the minimum cost
+	var dp func(pos, mask int) float64
+	dp = func(pos, mask int) float64 {
+		if mask == (1<<n)-1 {
+			return dist[pos][0]
+		}
+		if memo[pos][mask] != -1 {
+			return memo[pos][mask]
+		}
+		minCost := MAX
+		for city := 0; city < n; city++ {
+			if city != pos && (mask&(1<<city)) == 0 {
+				newCost := dist[pos][city] + dp(city, mask|(1<<city))
+				if newCost < minCost {
+					minCost = newCost
+				}
+			}
+		}
+		memo[pos][mask] = minCost
+		return minCost
+	}
+
+	minDistance := dp(0, 1)
+
+	// Function to reconstruct the path
+	findPath := func() []int {
+		mask := 1
+		pos := 0
+		path := []int{0}
+
+		for i := 1; i < n; i++ {
+			bestCity := -1
+			bestCost := MAX
+			for city := 0; city < n; city++ {
+				if (mask & (1 << city)) == 0 {
+					currentCost := dist[pos][city] + memo[city][mask|(1<<city)]
+					if currentCost < bestCost {
+						bestCost = currentCost
+						bestCity = city
+					}
+				}
+			}
+			path = append(path, bestCity)
+			pos = bestCity
+			mask |= 1 << bestCity
+		}
+		path = append(path, 0)
+		return path
+	}
+
+	minPath := findPath()
+	return minDistance, minPath
+}
+
 type MyEvent struct {
 	DistanceMatrix string `json:"distance_matrix"`
 	NumberOfPoints int32  `json:"number_of_points"`
